ed51b7a5ca72a1228add80fbd66dfb3e8417683a
Dan Kiuna <dkkiuna11@gmail.com>
2023-02-08 11:05:51 -0600

    working SSR with Auth

--- app.vue
+++ app.vue
@@ -2,18 +2,15 @@
 import { Authenticator } from "@aws-amplify/ui-vue";
 import { withSSRContext } from "aws-amplify";
 import "@aws-amplify/ui-vue/styles.css";
-console.log('test')
+
 const { data } = await useAsyncData('authinfo', async () => {
-
   const { node: { req } } = useRequestEvent();
-
   const { Auth } = withSSRContext({ req });
   try {
     const data = await Auth.currentAuthenticatedUser();
-    console.log('data',data)
     return data;
   } catch(e){
-    console.log('error',e)
+    // user not authenticated or other errors
     return e
   }
 });
@@ -24,7 +21,6 @@ const { data } = await useAsyncData('authinfo', async () => {
   <p>From SSR: {{ data?.attributes ? data.attributes : data }}</p>
   <hr />
   <Authenticator>
-    
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
